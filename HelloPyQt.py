d71a1c0070c3959df73842bc376b0ffa6a0de5b6
Gatsjy <gkswndks123@naver.com>
2020-11-22 12:36:04 +0900

    2020-11-22, 한주안, 호싸 스톡 프로젝트 첫번째 commit

---
+++ HelloPyQt.py
@@ -0,0 +1,15 @@
+"""
+  * @author Gatsjy
+  * @since 2020-11-22
+  * realize dreams myself
+  * Blog : https://blog.naver.com/gkswndks123
+  * Github : https://github.com/gatsjy
+"""
+
+import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)
+label = QLabel("Hello PyQt")
+label.show()
+app.exec_()
