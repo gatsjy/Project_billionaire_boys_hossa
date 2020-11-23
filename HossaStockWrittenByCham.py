98de058821f217823a6b74f7691085bf684e8d7a
kkc2886 <74913608+kkc2886@users.noreply.github.com>
2020-11-23 22:45:14 +0900

    Update HossaStockWrittenByCham.py
    1. 프로그램 구동 시점  뉴스 크롤링 시 -1일 기준 추가
    2. 월요일 날 구동 시 금요일~일요일 뉴스 검색 조건 추가

--- HossaStockWrittenByCham.py
+++ HossaStockWrittenByCham.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime,date
 import pandas as pd
 import os
 import random
 import time
 
-print(Test)
+#Today는 금일 날짜, dayOfTheWeek,D_day는 전일 요일,날짜
+def printDayOfTheWeek (today):
+    dayOfTheWeek = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
+    year = int(today[0:4])
+    month = int(today[5:7])
+    day =  int(today[-2:])-1
+    D_day = f"{year}.{month}.{day}"
+    D_day_3_ago = f"{year}.{month}.{day-2}"
+    return dayOfTheWeek[date(year, month, day).weekday()],D_day,D_day_3_ago
 
 if __name__ == "__main__":
 
@@ -18,35 +26,63 @@ if __name__ == "__main__":
 
     ##날짜 갖고오기 (2020.11.19)
     today = datetime.today().strftime("%Y.%m.%d")
-
+    dayOfTheWeek,D_day,D_day_3_ago = printDayOfTheWeek (today)
     not_mentioned_stock_list = []
 
     ##############################test용 설정 ####################################
     stock_list = stock_list[:10]
     #stock_num_list = stock_num_list[:10]
     ##############################################################################
+    
+    #월요일날 실행시 금요일,토요일,일요일 뉴스 갖고오기
+    if dayOfTheWeek == "일요일":
+        for stock in stock_list["회사명"], stock_list["종목코드"]:
+            url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={D_day_3_ago}&de={D_day}&mynews=0&refresh_start=0&related=0"
+            raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        
+            #delay_time = random.random() + random.random()
+            #time.sleep(delay_time)
+            # requests응답이 없을 경우.
+            if raw.status_code != requests.codes.ok:
+                print("접속실패")
 
-    for stock in stock_list["회사명"], stock_list["종목코드"]:
-        url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={today}&de={today}&mynews=0&refresh_start=0&related=0"
-        raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+            html = BeautifulSoup(raw.text, "html.parser")
+            news_wrappers = html.select('ul.list_news > li')
 
-        #delay_time = random.random() + random.random()
-        #time.sleep(delay_time)
-        # requests응답이 없을 경우.
-        if raw.status_code != requests.codes.ok:
-            print("접속실패")
+            cnt = 0
 
-        html = BeautifulSoup(raw.text, "html.parser")
-        news_wrappers = html.select('ul.list_news > li')
+            for resources in news_wrappers:
+                title = resources.select_one("a.news_tit").text
+                cnt += 1
 
-        cnt = 0
+            if cnt == 0:
+                not_mentioned_stock_list.append(stock)
 
-        for resources in news_wrappers:
-            title = resources.select_one("a.news_tit").text
-            cnt += 1
+        print(not_mentioned_stock_list[1])
 
-        if cnt == 0:
-            not_mentioned_stock_list.append(stock)
+        
+    else : 
+        for stock in stock_list["회사명"], stock_list["종목코드"]:
+            url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={today}&de={today}&mynews=0&refresh_start=0&related=0"
+            raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-    print(not_mentioned_stock_list[1])
+            #delay_time = random.random() + random.random()
+            #time.sleep(delay_time)
+            # requests응답이 없을 경우.
+            if raw.status_code != requests.codes.ok:
+                print("접속실패")
+
+            html = BeautifulSoup(raw.text, "html.parser")
+            news_wrappers = html.select('ul.list_news > li')
+
+            cnt = 0
+
+            for resources in news_wrappers:
+                title = resources.select_one("a.news_tit").text
+                cnt += 1
+
+            if cnt == 0:
+                not_mentioned_stock_list.append(stock)
+
+        print(not_mentioned_stock_list[1])
 
