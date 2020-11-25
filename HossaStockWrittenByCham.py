4f5cd8e355fb9ec16159ddc9ff9f3ad02b24453f
kkc2886 <74913608+kkc2886@users.noreply.github.com>
2020-11-26 00:54:53 +0900

    기본종목데이터 + 조건1. 동전주/스택주/우선주 + 조건2. 뉴스 크롤링 (해당 코드 구동 시간 : 30분)
    코드가 깔끔하진 않아서, 보기 편하게 최대한 수정해봄.
    추가로 3번 조건부터 같이 진행합시다.

--- HossaStockWrittenByCham.py
+++ HossaStockWrittenByCham.py
@@ -5,16 +5,17 @@ import pandas as pd
 import os
 import random
 import time
+import FinanceDataReader as fdr
 
 #Today는 금일 날짜, dayOfTheWeek,D_day는 전일 요일,날짜
 def printDayOfTheWeek (today):
     dayOfTheWeek = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
     year = int(today[0:4])
     month = int(today[5:7])
-    day =  int(today[-2:])-1
-    D_day = f"{year}.{month}.{day}"
-    D_day_3_ago = f"{year}.{month}.{day-2}"
-    return dayOfTheWeek[date(year, month, day).weekday()],D_day,D_day_3_ago
+    day =  int(today[-2:])
+    D_day_1_ago = f"{year}.{month}.{day-1}"
+    D_day_3_ago = f"{year}.{month}.{day-3}"
+    return dayOfTheWeek[date(year, month, day).weekday()],D_day_1_ago,D_day_3_ago
 
 if __name__ == "__main__":
 
@@ -22,22 +23,38 @@ if __name__ == "__main__":
     ###### 변경해야 될 input data = 코스닥 excel########
     ####################################################
     stock_df = pd.read_excel("./resource/코스닥.xlsx")
+    stock_df['종목코드'] = stock_df['종목코드'].apply(lambda x: "{:0>6d}".format(x)) #종목코드 string변환
     stock_list = pd.DataFrame(stock_df, columns=["회사명", "종목코드"])
 
     ##날짜 갖고오기 (2020.11.19)
     today = datetime.today().strftime("%Y.%m.%d")
-    dayOfTheWeek,D_day,D_day_3_ago = printDayOfTheWeek (today)
-    not_mentioned_stock_list = []
-
-    ##############################test용 설정 ####################################
-    stock_list = stock_list[:10]
-    #stock_num_list = stock_num_list[:10]
-    ##############################################################################
+    dayOfTheWeek,D_day_1_ago,D_day_3_ago = printDayOfTheWeek(today)
     
+    
+    ##조건0. OUTPUT = raw_step_data, 코스닥 종목+종가 가격 데이터
+    Close_list = [fdr.DataReader(f"{x}",D_day_1_ago,D_day_1_ago)["Close"] for x in stock_list["종목코드"]]
+    raw_df=pd.DataFrame(Close_list)
+    Close_df=pd.DataFrame(raw_df.values, columns=[f"{D_day_1_ago}_종가"])
+    raw_step_data = pd.concat([stock_list,Close_df],axis=1)                      #조건0. OUTPUT = raw_step_data
+    print("raw 조건 종목 개수 :",len(raw_step_data))
+    
+    
+    ##조건1. OUTPUT = first_step_data, 동전주 1200이상 적용
+    first_step_data=raw_step_data[raw_step_data[f"{D_day_1_ago}_종가"] >= 1200]  #조건1. OUTPUT = first_step_data
+    print("첫번째 조건 종목 개수 :",len(first_step_data),"_동전주 1200이상 적용")
+    
+    
+    ##############################test용 설정 ####################################
+    raw_step_data = raw_step_data[:10]
+    ##############################################################################
+   
+
+    ##조건2. OUTPUT = second_step_data, 뉴스 크롤링, 전날 뉴스 0건인 자료
+    not_mentioned_stock_list = []
     #월요일날 실행시 금요일,토요일,일요일 뉴스 갖고오기
-    if dayOfTheWeek == "일요일":
-        for stock in stock_list["회사명"], stock_list["종목코드"]:
-            url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={D_day_3_ago}&de={D_day}&mynews=0&refresh_start=0&related=0"
+    if dayOfTheWeek == "월요일":
+        for stock in stock_list["회사명"]:
+            url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={D_day_3_ago}&de={D_day_1_ago}&mynews=0&refresh_start=0&related=0"
             raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         
             #delay_time = random.random() + random.random()
@@ -58,12 +75,12 @@ if __name__ == "__main__":
             if cnt == 0:
                 not_mentioned_stock_list.append(stock)
 
-        print(not_mentioned_stock_list[1])
+        print(not_mentioned_stock_list[:])
 
         
     else : 
-        for stock in stock_list["회사명"], stock_list["종목코드"]:
-            url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={D_day}&de={D_day}&mynews=0&refresh_start=0&related=0"
+        for stock in stock_list["회사명"]:
+            url = f"https://search.naver.com/search.naver?where=news&query={stock}&sm=tab_opt&sort=0&photo=0&field=1&reporter_article=&pd=3&ds={D_day_1_ago}&de={D_day_1_ago}&mynews=0&refresh_start=0&related=0"
             raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
             #delay_time = random.random() + random.random()
@@ -84,5 +101,9 @@ if __name__ == "__main__":
             if cnt == 0:
                 not_mentioned_stock_list.append(stock)
 
-        print(not_mentioned_stock_list[1])
-
+        print(not_mentioned_stock_list[:])
+        
+   
+    print("두번째 조건 종목 개수 :",len(not_mentioned_stock_list),"_뉴스크롤링")
+    isin_filter =first_step_data["회사명"].isin(not_mentioned_stock_list)
+    second_step_data = first_step_data[isin_filter]                                 #조건2. OUTPUT = second_step_data
